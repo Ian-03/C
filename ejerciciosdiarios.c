952bf2a3fd5b8f64c249dfb27bf3bf56a25cee4e
Ian-03 <78566383+Ian-03@users.noreply.github.com>
2021-02-11 15:49:42 -0600

    Add files via upload

---
+++ ejerciciosdiarios.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    int fahrenheit=0, celsius;
+
+    printf("This is a table that compares the temperature between fahrenheit adn celsius: \n");
+    
+    while(fahrenheit<=300){
+        
+        celsius=5*(fahrenheit-32)/9;
+
+        printf("%d F    %d C\n", fahrenheit, celsius);
+        fahrenheit+=20;
+    }
+
+    return 0;
+}
