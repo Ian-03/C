4e887e6a12bf2b0b67420331105f986bb02988d7
Ian-03 <78566383+Ian-03@users.noreply.github.com>
2021-03-10 20:09:40 -0600

    Add files via upload

---
+++ Chapter2/Exercise2_5.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+void getline (char [],int);
+
+char strpbrkz (char [], char []);
+
+int main (){
+
+const int MAX=1000;
+
+char line1[MAX];
+char line2[MAX];
+
+    getline (line1,MAX);
+    getline (line2,MAX);
+
+    strpbrkz (line1,line2);
+
+    return 0;
+}
+ 
+char strpbrkz (char line1[], char line2[]){
+
+    int j,i,c=0;
+    char f;
+
+    for (i=0; line1[i]!='\0'; i++){
+        if (c==0){
+          for (j=0; line2[j]!='\0'; j++) {
+             if (line2[j]==line1[i]){
+                  f=line1[i];
+                  c++;
+                  break;
+              }
+            }  
+        }
+        else break;
+    }
+
+    if(c==0){
+        return -1;
+    }
+    else{
+        printf("%c",f);
+        } 
+
+}
+
+void getline (char line[], int MAX){
+
+    int i,c;
+    
+    for (i=0; i<MAX-1 && (c=getchar())!='\n' && c!=EOF; i++){
+        line[i]=c;
+    } 
+    if (c=='\n'){
+        line[i++]=c;
+        } 
+    line[i]='\0';
+}
