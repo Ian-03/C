d7e6aa3891765c45c8c46e7bf4978654556f4432
Ian-03 <78566383+Ian-03@users.noreply.github.com>
2021-03-13 18:16:33 -0600

    Add files via upload

---
+++ Chapter3/Exercise3_2.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void getline (char []);
+
+void escape (char [], char []);
+
+int main(){
+
+    const int MAX=1000;
+    char s[MAX], t[MAX];
+
+    getline (s);
+    escape (t,s);
+
+    printf("%s",t);
+    
+    return 0;
+}
+
+void getline (char s[]){
+
+    int i=0,c;
+
+    while((c=getchar()) != EOF){
+        s[i++]=c;
+    }
+}
+
+void escape (char t[], char s[]){
+
+    int i=0,j=0;
+
+    while (s[i] != '\0'){  
+        if (s[i]=='\n'){
+            t[j++]='\\';
+            t[j++]='n';
+        }else if (s[i]=='\t'){
+            t[j++]='\\';
+            t[j++]='t';
+        }else t[j++]=s[i];
+        i++;
+        
+    }
+}
