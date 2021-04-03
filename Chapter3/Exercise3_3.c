fb96cf18ad0b058055be3757b085184184976ba1
Ian-03 <78566383+Ian-03@users.noreply.github.com>
2021-04-02 19:44:08 -0600

    Add files via upload

---
+++ Chapter3/Exercise3_3.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<string.h>
+
+void getline (char[], int);
+
+void expand (char [],char []);
+
+int main(){
+  
+  const int max=1000;
+  char s1[max],s2[max];
+
+  getline(s1,max);
+
+  expand (s1,s2);
+
+  return 0;
+}
+
+void expand (char s1[], char s2[]){
+
+  int j=0;
+
+  for (int i=0; i<strlen(s1); i++){
+  
+    if (s1[i]=='-' && i!=0 && i!=strlen(s1)-1){
+
+      do{
+         s1[i-1]++;
+         s2[j++]=s1[i-1];
+      }while (s1[i-1]!=s1[i+1]);
+
+      i++;
+
+    }else s2[j++]=s1[i];
+  }
+
+  printf("%s",s2);
+}
+
+void getline (char s1[], int max){
+
+  int c,i;
+
+  for(i=0; i<max-1 && (c=getchar())!='\n' && c!=EOF; i++){
+    s1[i]=c;
+  }
+  if(c=='\n'){
+    s1[i++]='\n';
+  }
+  s1[i]='\0';
+
+}
